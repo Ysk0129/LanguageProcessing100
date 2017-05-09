bd77d892919d29fd14fe32fa45e54cf701921e0b
Yoshid01 <garlic.ba.0129@gmail.com>
2017-05-10 00:42:13 +0900

    Fix directory

--- LanguageProcessing100/chapter1/knock00.py
+++
@@ -1,6 +0,0 @@
-
-if __name__ == '__main__':
-    str = "stressed"
-    answer = str[::-1]
-    
-    print(answer)
--- LanguageProcessing100/chapter1/knock01.py
+++
@@ -1,6 +0,0 @@
-
-if __name__ == "__main__":
-    str = "パタトクカシー"
-    answer = str[::2]
-
-    print(answer)
