01ed85a442102e69e120db4a6655321f0b77a21d
Yoshid01 <garlic.ba.0129@gmail.com>
2017-05-14 16:17:57 +0900

    Delete unnecessary code

--- LanguageProcessing100/chapter1/knock04.py
+++ LanguageProcessing100/chapter1/knock04.py
@@ -1,6 +1,5 @@
 
 if __name__ == "__main__":
-    print(dir(""))
     sentence = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
     words = sentence.split()
     one_letter_cases = [1, 5, 6, 7, 8, 9, 15, 16, 19]
