977b5c5b2538db06fc27440b829e09d781b284eb
Yoshid01 <garlic.ba.0129@gmail.com>
2017-05-20 15:16:55 +0900

    Fixed as simple code

--- LanguageProcessing100/chapter2/knock11.py
+++ LanguageProcessing100/chapter2/knock11.py
@@ -1,10 +1,5 @@
 
 if __name__ == "__main__":
 
-    txt = ""
     with open("hightemp.txt") as file:
-        txt = file.read()
-    
-    answer = txt.replace("\t", " ")
-
-    print(answer)
+        print(file.read().replace("\t", " ")
