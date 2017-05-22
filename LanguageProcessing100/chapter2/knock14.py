a24ea03af308d1cb2f7e917bf2ae96fb0a03694a
Yoshid01 <garlic.ba.0129@gmail.com>
2017-05-23 00:22:42 +0900

    Add knock 14

---
+++ LanguageProcessing100/chapter2/knock14.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+if __name__ == "__main__":
+
+    with open("hightemp.txt") as file:
+        lines = file.readlines()[:int(argv[1])]
+
+    for line in lines:
+        print(line.rstrip("\n"))
