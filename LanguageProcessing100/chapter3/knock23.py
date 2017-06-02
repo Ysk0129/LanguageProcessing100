f655296e7d9e779495555783d4d2d64d3fd09835
Yoshid01 <garlic.ba.0129@gmail.com>
2017-06-03 00:11:51 +0900

    Add knock 23

---
+++ LanguageProcessing100/chapter3/knock23.py
@@ -0,0 +1,14 @@
+import re
+
+if __name__ == "__main__":
+
+    with open("british_articles.txt") as file:
+        lines = file.readlines()
+    
+    section_pattern = re.compile("^(=+)\s*(.+?)\s*(=+)$")
+    for line in lines:
+        section = re.search(section_pattern, line)
+        if section is not None:
+            section_name = section.group(2)
+            section_level = str(len(section.group(1)) - 1)
+            print(section_name + " : " + section_level)
