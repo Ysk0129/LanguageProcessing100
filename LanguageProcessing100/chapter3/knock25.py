16b601f55a4bf9aed9fc0b0bd77cae911bfb0e4e
Yoshid01 <garlic.ba.0129@gmail.com>
2017-06-06 00:38:36 +0900

    Add knock 25

---
+++ LanguageProcessing100/chapter3/knock25.py
@@ -0,0 +1,21 @@
+import re
+
+def extract_basic_info(text):
+    basic_info_pattern = re.compile(r"^\{\{基礎情報.*?\n(^|.*?)^\}\}", re.MULTILINE + re.DOTALL)
+    basic_info = re.search(basic_info_pattern, text).group(1)
+    return basic_info
+
+def extract_field_dict(text):
+    field_pattern = re.compile(r"^\|(.*?)\s\=\s(.*?)(?=\n\|)", re.MULTILINE + re.DOTALL)
+    field_dict = dict(field_pattern.findall(text))
+    return field_dict
+
+if __name__ == "__main__":
+    with open("british_articles.txt") as file:
+        text = file.read()
+
+    basic_info = extract_basic_info(text)
+    extract_field = extract_field_dict(basic_info)
+
+    for k, v in extract_field.items():
+        print(k + " = " + v)
