9a4360603f550aa5ca436d2dc90e4cc4b07de9ff
Yoshid01 <garlic.ba.0129@gmail.com>
2017-06-08 21:11:09 +0900

    Fix trimming strong markup as a function

--- LanguageProcessing100/chapter3/knock26.py
+++ LanguageProcessing100/chapter3/knock26.py
@@ -1,6 +1,11 @@
 import knock25
 import re
 
+def trim_strong(text):
+    strong_pattern = re.compile(r"'{2,5}")
+    trimmed = re.sub(strong_pattern, "", text)
+    return trimmed
+
 if __name__ == "__main__":
     with open("british_articles.txt") as file:
         text = file.read()
@@ -8,9 +13,7 @@ if __name__ == "__main__":
     basic_info = knock25.extract_basic_info(text)
     field_pairs = knock25.extract_field_dict(basic_info)
 
-    strong_pattern = re.compile(r"'{2,5}")
-
     for k, v in field_pairs.items():
-        trimmed_k = re.sub(strong_pattern, "", k)
-        trimmed_v = re.sub(strong_pattern, "", v)
+        trimmed_k = trim_strong(k)
+        trimmed_v = trim_strong(v)
         print(trimmed_k + " = " + trimmed_v)
