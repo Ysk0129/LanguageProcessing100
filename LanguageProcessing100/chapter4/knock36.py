45091716eef4c2631bebdd864b62534455f5084a
Yoshid01 <garlic.ba.0129@gmail.com>
2017-06-24 12:35:50 +0900

    Fix method argument to variable length

--- LanguageProcessing100/chapter4/knock36.py
+++ LanguageProcessing100/chapter4/knock36.py
@@ -1,14 +1,14 @@
 import knock30
 import re
 
-def count_occurrences_words(morphemes, exclusion_pos_list, exclusion_condition):
+def count_occurrences_words(morphemes, **exclusions):
 
     word_occurrences_dict = {}
     for morpheme in morphemes:
-
-        if morpheme["pos"] in exclusion_pos_list:
+        
+        if "pos_list" in exclusions and morpheme["pos"] in exclusions["pos_list"]:
             continue
-        if exclusion_condition(morpheme["surface"]):
+        if "condition" in exclusions and exclusions["condition"](morpheme["surface"]):
             continue
 
         if morpheme["surface"] in word_occurrences_dict.keys():
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     morphemes = knock30.extract_morphemes("neko.txt.mecab")
     exclusion_pattern = re.compile("^[ぁ-ん]$")
     exclusion_func = lambda w: re.match(exclusion_pattern, w) is not None
-    word_occurrences_dict = count_occurrences_words(morphemes, ["助詞", "助動詞", "記号"], exclusion_func)
-
-    for k, v in sorted(word_occurrences_dict.items(), key=lambda x: x[1], reverse=True):
+    occurrences_dict = count_occurrences_words(morphemes, pos_list=["助詞", "助動詞", "記号"], condition=exclusion_func)
+    
+    for k, v in sorted(occurrences_dict.items(), key=lambda x: x[1], reverse=True):
         print(k + ":" + str(v))
