65ca2ee90c80421e7e0b4ac024a5854287e6191d
Yoshid01 <garlic.ba.0129@gmail.com>
2017-07-10 00:03:57 +0900

    Functionization and bug fixes

--- LanguageProcessing100/chapter5/knock41.py
+++ LanguageProcessing100/chapter5/knock41.py
@@ -21,17 +21,23 @@ def make_chunk_list(lattice_format_sentence):
             continue
         
         elements = line.replace("\t", ",").split(",")
-        if len(elements) == 10:
+        if len(elements) >= 8:
+            base = elements[7] if elements[7] != "*" else elements[0]
             morph = Morph(elements[0], elements[7], elements[1], elements[2])
             morphs.append(morph)
     if len(morphs) != 0: chunks.append(Chunk(srcs, dst, morphs))
 
     return chunks
 
-if __name__ == "__main__":
-
+def make_chunks_list(lattice_format_filename):
     chunks_list = []
-    sentences = knock40.group_by_sentence("neko.txt.cabocha")
+    sentences = knock40.group_by_sentence(lattice_format_filename)
     for sentence in sentences:
         chunks_list.append(make_chunk_list(sentence))
-    print([str(chunk) for chunk in chunks_list[2]])
+    return chunks_list
+
+if __name__ == "__main__":
+
+    chunks_list = make_chunks_list("neko.txt.cabocha")
+
+    print([str(chunk) for chunk in chunks_list[8]])
