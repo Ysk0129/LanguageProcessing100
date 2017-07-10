acc3d1d29a177fae0ebecca869bc62f85a43002b
Yoshid01 <garlic.ba.0129@gmail.com>
2017-07-11 00:45:19 +0900

    Add function to remove mark

--- LanguageProcessing100/chapter5/chunk.py
+++ LanguageProcessing100/chapter5/chunk.py
@@ -10,5 +10,7 @@ class Chunk:
     def __str__(self):
         return "srcs: {0}, dst: {1}, morphs: {2}".format(self.srcs, self.dst, [str(morph) for morph in self.morphs])
 
+    def remove_marks(self):
+        self.morphs = [morph for morph in self.morphs if not morph.is_mark()]
 
 CHUNK_LINE_PATTERN = re.compile(r"^\*\s(\d+)\s(-?\d+)D\s(\d+/\d+)+\s(-|\d|\.)+\n$")
