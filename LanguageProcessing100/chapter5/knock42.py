7fb90ff184bc62ec2aa7802884083ba650d76925
Yoshid01 <garlic.ba.0129@gmail.com>
2017-07-14 00:41:11 +0900

    Add knock 42

---
+++ LanguageProcessing100/chapter5/knock42.py
@@ -0,0 +1,24 @@
+import knock41
+
+def make_dependency_pairs(chunks):
+
+    dependency_pairs = []
+    for chunk in chunks:
+        if chunk.dst == "-1":
+            continue
+
+        chunk.remove_marks()
+        dst_chunk = chunks[int(chunk.dst)]
+        dst_chunk.remove_marks()
+
+        pair = (chunk, dst_chunk)
+        dependency_pairs.append(pair)
+
+    return dependency_pairs
+    
+if __name__ == "__main__":
+    chunks_list = knock41.make_chunks_list("neko.txt.cabocha")
+
+    for chunks in chunks_list:
+        for pair in make_dependency_pairs(chunks):
+            print(pair[0].get_phrase() + "\t" + pair[1].get_phrase())
