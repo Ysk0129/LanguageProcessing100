ade23e3d29f8a9e1512510b7d1e4ff277711d4d3
Yoshid01 <garlic.ba.0129@gmail.com>
2017-08-08 23:38:27 +0900

    Fix knock 50

--- LanguageProcessing100/chapter6/knock50.py
+++ LanguageProcessing100/chapter6/knock50.py
@@ -4,25 +4,33 @@ if __name__ == "__main__":
     with open("nlp.txt") as f:
         line = f.read()
 
-    first = re.compile(r"[\.;:\?\!]")
+    first = re.compile(r"[.;:?!]")
     second = re.compile(r"\s")
     third = re.compile(r"[A-Z]")
     three_chars = []
-    sentences = []
     sentence = ""
-    for ch in line.replace("\n", ""):
+
+    for ch in line.replace("\n", " ").replace("\t", " "):
+
         three_chars.append(ch)
+
         if len(three_chars) < 3:
-            sentence += ch
             continue
 
-        three_chars = three_chars[1:4]
+        if len(three_chars) > 3:
+            three_chars = three_chars[1:4]
+
+        if three_chars[1] == " " and three_chars[2] == " ":
+            three_chars = three_chars[0:3]
+            continue
+
         f = first.match(three_chars[0])
         s = second.match(three_chars[1])
-        t = third.match(three_chars[1])
-        if t is None or s is None or t is None:
+        t = third.match(three_chars[2])
+
+        if f is None or s is None or t is None:
             sentence += ch
         else:
             sentence.rstrip(" ")
             print(sentence)
-            sentence = ""
+            sentence = ch
