fcd42dceac08f3c4b493e650b98823ee86ef438a
Yoshid01 <garlic.ba.0129@gmail.com>
2017-08-10 01:20:37 +0900

    Fix knock 51

--- LanguageProcessing100/chapter6/knock51.py
+++ LanguageProcessing100/chapter6/knock51.py
@@ -1,9 +1,13 @@
 import sys
+import re
 
 if __name__ == "__main__":
 
+    term = re.compile(r"[.;:?!]")
     sentences = sys.argv[1].split("\n")
     for sentence in sentences:
-        words = sentence.split("\s")
-        for word in words:
-            print(word)
+        words = sentence.split(" ")
+        for i in range(0, len(words)):
+            print(words[i])
+            if i == len(words) -1:
+                print()
