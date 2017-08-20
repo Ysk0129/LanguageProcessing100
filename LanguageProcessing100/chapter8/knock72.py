1cabdd8c6cb976e563e388ae8b76140865d3235b
Yoshid01 <garlic.ba.0129@gmail.com>
2017-08-21 08:51:16 +0900

    Add knock 72 half way

--- LanguageProcessing100/chapter8/knock72.py
+++ LanguageProcessing100/chapter8/knock72.py
@@ -1,5 +1,11 @@
 import re
+from stopword import StopWord
+from stemming.porter2 import stem
 
 def trim_ignore_marks(word):
-    ignore_marks = re.compile(r"[^a-zA-Z!?]")
+    ignore_marks = re.compile(r"[^a-zA-Z!?']")
     return re.sub(ignore_marks, "", word)
+
+def preprocess_stems(stems):
+    sw = StopWord()
+    return [trim_ignore_marks(stm) for stm in stems if not sw.is_included_stem(stm)]
