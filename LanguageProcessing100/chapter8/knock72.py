0da60a6771c1f4c43e605a3f070abe031d6ada85
Yoshid01 <garlic.ba.0129@gmail.com>
2017-08-22 00:21:07 +0900

    Fix knock 72

--- LanguageProcessing100/chapter8/knock72.py
+++ LanguageProcessing100/chapter8/knock72.py
@@ -9,3 +9,21 @@ def trim_ignore_marks(word):
 def preprocess_stems(stems):
     sw = StopWord()
     return [trim_ignore_marks(stm) for stm in stems if not sw.is_included_stem(stm)]
+
+if __name__ == "__main__":
+
+    with open("sentiment.txt") as f:
+        lines = f.readlines()
+
+    words_list = [line.split(" ")[1:] for line in lines]
+    #flatten
+    words = [word for words in words_list for word in words]
+    #stemming処理に時間がかかるため、stemmingしない状態で先にuniqにする
+    uniq_words = list(set(words))
+    stems = [stem(word) for word in uniq_words]
+
+    features = list(set(preprocess_stems(stems)))
+    features.remove("")
+
+    with open("features.txt", "w") as f:
+        f.writelines("\n".join(features))
