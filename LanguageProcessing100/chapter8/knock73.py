583af8bf4ad203757b311ce146335e2ba870779d
Yoshid01 <garlic.ba.0129@gmail.com>
2017-08-25 00:32:51 +0900

    Add a function used by knock 73

--- LanguageProcessing100/chapter8/knock73.py
+++ LanguageProcessing100/chapter8/knock73.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def load_features():
     with open("features.txt") as f:
@@ -10,3 +11,7 @@ def load_sentiments():
         #改行を除去する
         lines = f.read().split("\n")
     return lines
+
+def sigmoid(data_x, theta):
+    print(np.exp(-data_x.dot(theta)))
+    return 1 / (1 + np.exp(-data_x.dot(theta)))
