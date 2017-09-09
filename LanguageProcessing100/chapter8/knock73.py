c707dfd408c6c7eda8411826c1e83a14dfed2348
Yoshid01 <garlic.ba.0129@gmail.com>
2017-09-09 22:14:33 +0900

    Fix knock 73

--- LanguageProcessing100/chapter8/knock73.py
+++ LanguageProcessing100/chapter8/knock73.py
@@ -15,7 +15,7 @@ def load_sentiments():
     return lines
 
 def sigmoid(data_x, theta):
-    print(np.exp(-data_x.dot(theta)))
+
     return 1 / (1 + np.exp(-data_x.dot(theta)))
 
 def stem_all(words):
@@ -29,7 +29,7 @@ def cost(data_x, theta, data_y):
     return prediction
 
 def gradient(data_x, theta, data_y):
-    size = data_y.size()
+    size = data_y.size
     hypothesis = sigmoid(data_x, theta)
     gradient = 1 / size * (hypothesis - data_y).dot(data_x)
     return gradient
@@ -39,14 +39,31 @@ def remove_stopword(stems):
     removed = [stm for stm in stems if not sw.is_included_stem(stm)]
     return removed
 
+def make_dict_features(features):
+    return {line: i for i, line in enumerate(features, start=1)}
+
+def extract_included_features(stems, dict_features):
+    data_x_element = np.zeros(len(dict_features) + 1, dtype=np.float64)
+    #先頭要素は素性に対応しない重み用のため1とする
+    data_x_element[0] = 1
+    for stm in stems:
+        if stm in dict_features.keys():
+            data_x_element[dict_features[stm]] = 1
+    return data_x_element
+
 def learn(data_x, data_y, alpha, count):
     theta = np.zeros(data_x.shape[1])
-    cost = cost(data_x, theta, data_y)
+    c = cost(data_x, theta, data_y)
+    print("学習開始")
+    print("cost: {}".format(c))
 
     for i in range(1, count + 1):
-        gradient = gradient(data_x, theta, data_y)
-        theta -= alpha * gradient
+        g = gradient(data_x, theta, data_y)
+        theta -= alpha * g
 
+    c = cost(data_x, theta, data_y)
+    print("学習完了")
+    print("cost: {}".format(c))
     return theta
 
 if __name__ == "__main__":
@@ -56,4 +73,29 @@ if __name__ == "__main__":
 
     splited = [sentiment.split(" ") for sentiment in sentiment_lines]
     sentiments = [line[1:] for line in splited]
+
+    #各行をそれぞれstemmingしたもの
+    sentiments_stems = [stem_all(sentiment) for sentiment in sentiments]
+    #正解ラベル
     labels = [line[0] for line in splited]
+
+    dict_features = make_dict_features(features)
+
+    data_x = np.zeros([len(sentiments_stems), len(dict_features) + 1], dtype=np.float64)
+    data_y = np.zeros(len(labels), dtype=np.float64)
+
+    for i, stems in enumerate(sentiments_stems):
+        data_x[i] = extract_included_features(stems, dict_features)
+
+        #labelの値が-1のときは0(デフォルト),+1のときは1とする
+        #sentiments_stemsとlabelはsentimentsの1行に対応するので長さは同じ
+        if labels[i] == "+1":
+            data_y[i] = 1
+    
+    #学習率
+    alpha = 6.0
+    #学習の繰り返し数
+    count = 1000
+    theta = learn(data_x, data_y, alpha, count)
+
+    np.save("theta.npy", theta)
